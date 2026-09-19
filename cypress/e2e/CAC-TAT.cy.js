2ad6029d379901422a1bd308d116b46530cd3dd7
GabrielPiresC <barbogabriel@gmail.com>
2026-09-19 00:54:28 -0300

    Correção exercícios aula 7

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -215,12 +215,10 @@ it('acessa a página da política de privacidade removendo o target e então cli
 })
 
 // Exercício extra 2 Aula 7
+// privacyPolicy.cy.js
 
-it.only('testa a página da política de privacidade de forma independente', () => {
-    cy.visit('./src/privacy.html')
+// Exercício Aula 8
 
-    cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
-    cy.contains('p', 'Talking About Testing').should('be.visible')
-})    
+
 
 })
